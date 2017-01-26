8d44d2b7b350cc147aab56161323851a613a5c69
Tai Le Huu <tle@blurb.com>
2017-01-26 15:10:39 +0700

    pagination

--- wp-content/themes/theme01/index.php
+++ wp-content/themes/theme01/index.php
@@ -1,18 +1,24 @@
 <?php get_header(); ?>
 
-<div id="content">
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <h2><?php the_title(); ?></h2>
-        <?php the_post_thumbnail(); ?>
-        <?php the_excerpt(); ?>
-    <?php endwhile;
-    else: ?>
-        <?php _e('Sorry, no posts matched your criteria.', 'textdomain'); ?>
-    <?php endif; ?>
-</div>
+    <div id="content">
+        <?php if (have_posts()) : ?>
+            <!-- Start of the main loop. -->
+            <?php while (have_posts()) : the_post(); ?>
+                <h2><?php the_title(); ?></h2>
+                <?php the_post_thumbnail(); ?>
+                <?php the_excerpt(); ?>
+            <?php endwhile; ?>
+            <!-- End of the main loop -->
 
-<div id="sidebar">
-    <?php get_sidebar(); ?>
-</div>
+            <!-- Add the pagination functions here. -->
+            <?php posts_nav_link(); ?>
+        <?php else : ?>
+            <?php _e('Sorry, no posts matched your criteria.', 'textdomain'); ?>
+        <?php endif; ?>
+    </div>
+
+    <div id="sidebar">
+        <?php get_sidebar(); ?>
+    </div>
 
 <?php get_footer(); ?>
