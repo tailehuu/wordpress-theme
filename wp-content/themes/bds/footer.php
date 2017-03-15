c74dc10184a5fbb56da316d0306db5c20fb6cac7
Tai Le Huu <tle@blurb.com>
2017-03-15 18:42:35 +0700

    small CSS change

--- wp-content/themes/bds/footer.php
+++ wp-content/themes/bds/footer.php
@@ -10,7 +10,7 @@
 
 ?>
 
-<footer>
+<footer <?= is_category() ? 'class="no-padding"' : ''?>>
     <div class="container">
         <div class="row">
             <div class="col-lg-10 col-lg-offset-1 text-center">
